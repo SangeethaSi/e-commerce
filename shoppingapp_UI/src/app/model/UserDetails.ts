e1495dd7d2b7339cca47f490cff97d1cf15c75d7
SangeethaSi <129830525+SangeethaSi@users.noreply.github.com>
2023-04-04 16:05:42 +0530

    Delete shoppingapp_UI directory

--- shoppingapp_UI/src/app/model/UserDetails.ts
+++
@@ -1,9 +0,0 @@
-export interface UserDetails {
-    firstName?: string,
-    lastName?: string,
-    email?: string,
-    loginId?: string,
-    password?: string,
-    contactNumber?: string,
-    role?: string
-}
